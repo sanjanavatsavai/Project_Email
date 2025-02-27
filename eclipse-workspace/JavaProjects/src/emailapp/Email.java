fac404a6b8897ec400511fcd2d8cab448dd0fde3
sanjanavatsavai <sanju82@outlook.com>
2025-02-27 11:29:59 -0500

    random password

--- eclipse-workspace/JavaProjects/src/emailapp/Email.java
+++ eclipse-workspace/JavaProjects/src/emailapp/Email.java
@@ -6,6 +6,7 @@ public class Email {
 	private String firstName;
 	private String lastName;
 	private String password;
+	private int defualtPasswordLength =10;
 	private String department;
 	private int mailCapacity;
 	private String alternateEmail;
@@ -17,6 +18,7 @@ public class Email {
 		System.out.println( "Email Created: "+ this.firstName+ " "+ this.lastName);
 		this.department = setDepartment();
 		System.out.println("Your Department is : "+this.department);
+		this.password = randomPassword(defaultPasswordLength);
 		
 	}
 	
@@ -30,6 +32,20 @@ public class Email {
 		else if(deptCode==3) {return "marketing";}
 		else {return "";}
 	}
+	
+	//generating random password
+		private String randomPassword(int length) {
+			String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+			char[] password = new char[length];
+			for(int i=0;i<length;i++) {
+				int random = (int) (Math.random()*passwordSet.length());	
+				password[i] = passwordSet.charAt(random);
+						
+			}
+			return new String(password);
+			
+			
+		}
 	
 
 }
--- eclipse-workspace/JavaProjects/src/emailapp/EmailApp.java
+++ eclipse-workspace/JavaProjects/src/emailapp/EmailApp.java
@@ -4,7 +4,7 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Email em1 =  new Email("John", "Smith");
+		Email em1 =  new Email("Jane", "Doe");
 		
 		
 		
